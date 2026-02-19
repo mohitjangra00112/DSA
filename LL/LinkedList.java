77f74510506764f95c724c41d980606a2cfd703a
mohitjangra00112 <mohitjangra00112@gmail.com>
2026-02-19 08:35:09 +0530

    Linked-List-Basic

---
+++ LL/LinkedList.java
@@ -0,0 +1,17 @@
+public class LinkedList {
+    
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+
+        public static Node Head;
+        public static Node Tail;
+
+    }
+
+}
