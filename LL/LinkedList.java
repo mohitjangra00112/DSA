72c6f531e1b188f2e45980cd0e6a93c0d8be9a40
mohitjangra00112 <mohitjangra00112@gmail.com>
2026-02-19 22:00:05 +0530

    Linked List Add Fiest

--- LL/LinkedList.java
+++ LL/LinkedList.java
@@ -8,9 +8,37 @@ public class LinkedList {
             this.data = data;
             this.next = null;
         }
+    }
 
-        public static Node Head;
+    public static Node Head;
         public static Node Tail;
+
+        public void addFirst(int data){
+
+            // Step 1 Create new node 
+           
+            Node newNode = new Node(data);
+            if(Head == null){
+                 Head =Tail = newNode;
+                 return;
+            }
+
+            // Step 2 new Node next = Head 
+
+            newNode.next = Head ; 
+
+            // Step 3  Head = new Node 
+
+            Head = newNode ; 
+        }
+
+    public static void main(String args[]){
+         
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+
+
 
     }
 
---
+++ Notes.md
@@ -0,0 +1,13 @@
+
+#  Add in Linked List 
+
+# add First 
+
+steps 
+1.  create new node
+2. new node next = head 
+3. head = new node
+
+// if there is no new node then 
+1. create new node 
+2. Head = Tail  = new Node
