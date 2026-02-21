4e171c2243159ffc8dceefd26a069dfcb0bebcdf
mohitjangra00112 <mohitjangra00112@gmail.com>
2026-02-21 13:54:26 +0530

    Linked-List add and remove

--- LL/LinkedList.java
+++ LL/LinkedList.java
@@ -11,12 +11,13 @@ public class LinkedList {
     }
 
     public static Node Head;
-        public static Node Tail;
+    public static Node Tail;
+    public static int size;     /// for size of LL
 
         public void addFirst(int data){
 
             // Step 1 Create new node 
-           
+            size++;
             Node newNode = new Node(data);
             if(Head == null){
                  Head =Tail = newNode;
@@ -32,14 +33,112 @@ public class LinkedList {
             Head = newNode ; 
         }
 
+        public void addLast(int data){
+            
+            size++;
+
+            //  Step 1  create new Node
+            
+            Node newNode = new Node(data);
+
+            if(Head == null){
+                Head = Tail = newNode;
+            }
+
+            // Step 2 Tail.next = newNode
+
+            Tail.next = newNode;
+
+            // Step 3 Tail = newNode
+
+            Tail = newNode;
+
+        }
+
+        //// Add Middle ( 0  based indexing )
+        
+        public void add(int index , int data){
+            
+            if(index ==0 ){         // if index is 0 , then we need to update the head . 
+                addFirst(data);
+            }
+            
+            size++;
+            Node newNode = new Node(data);
+            Node temp = Head;
+            int i = 0;
+
+            while( i < index-1 ){
+                temp = temp.next;
+                i++;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+        }
+
+        public int removeFirst(){
+            if(size==0){
+                System.out.print("LL is empty");
+                return 0; 
+            }else if (size==1){
+                int val = Head.data;
+                Head=Tail=null;
+                size=0;
+                return val;
+            }
+            int val = Head.data;
+            Head = Head.next;
+            size--;
+            return val;
+        }
+
+        public int removeLast(){
+             if(size==0){
+                System.out.print("LL is empty");
+                return 0; 
+            }else if (size==1){
+                int val = Head.data;
+                Head=Tail=null;
+                size=0;
+                return val;
+            }
+            // prev : i = size -2
+            Node prev = Head;
+
+            for(int i=0;i<size-2 ; i++){
+                prev = prev.next;
+            }
+
+            int val = prev.data;   // Tail data
+            prev.next = null;
+            Tail = prev;
+            size--;
+            return val;
+        }
+
+        public void print(){
+            Node temp = Head;
+
+            while(temp != null){
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+        }
+
     public static void main(String args[]){
          
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
         ll.addFirst(2);
-
-
-
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);   // 1 -> 2 -> 3 -> 4
+        ll.add(2,6);  // 1 -> 2 -> 6 -> 3 -> 4
+        
+        ll.print();
+        System.out.println();
+        System.out.println(size);
     }
 
 }
--- Notes.md
+++ Notes.md
@@ -1,13 +1,86 @@
 
 #  Add in Linked List 
 
-# add First 
+# add First O(1)
 
 steps 
-1.  create new node
+1. create new node
 2. new node next = head 
 3. head = new node
 
 // if there is no new node then 
 1. create new node 
 2. Head = Tail  = new Node
+
+# add Last 
+
+steps
+1. create new node
+2. tail.next = new node
+3. Tail = new Node
+
+// if there is no new node then 
+1. create new node 
+2. Head = Tail  = new Node
+
+# Print Linked List
+
+steps
+1. created temp Node  Node temp = Head;
+2. Run while loop while( temp != null )
+3. print temp.data  
+4. temp = temp.next
+
+#  Print Function   
+public void print(){
+            Node temp = Head;
+
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
+
+#  Add Middle 
+
+steps
+1. Create new Node
+2. create temp node , temp = Head
+3. int i = 0 ;  run while loop until we reach prev node  while( i < index-1)
+4. in loop ->  temp = temp.next  ,  i++
+5. After loop  newNode next  =  temp.next   and  temp.next = newNode
+
+# Add Function  
+public void add(int index , int data){
+
+            if(index ==0 ){         // if index is 0 , then we need to update the head . 
+                addFirst(data);
+            }
+
+            Node newNode = new Node(data);
+            Node temp = Head;
+            int i = 0;
+
+            while( i < index-1 ){
+                temp = temp.next;
+                i++;
+            }
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+        }
+
+# Remove First 
+steps
+1. If size == 0 , return 
+2. If size == 1  head=Tail=null
+3. Else Head = Head.next;
+
+# Remove Last
+steps
+1. If size == 0 , return 
+2. If size == 1  head=Tail=null
+3. Else  Temp -> Treverse till size-2
+4. Temp.next = null ,  Tail = Temp  
+5. See function removeLast for more understanding
